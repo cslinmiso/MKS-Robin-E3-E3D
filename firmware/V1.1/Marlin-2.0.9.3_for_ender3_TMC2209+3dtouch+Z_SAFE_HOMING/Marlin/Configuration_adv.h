39a36283e7569dada27d52088a66c26f6ab22ea8
weihsuehl <weihsuehl@yahooinc.com>
2023-03-17 14:32:51 +0800

    apply https://github.com/MarlinFirmware/Configurations/blob/release-2.0.9.3

--- firmware/V1.1/Marlin-2.0.9.3_for_ender3_TMC2209+3dtouch+Z_SAFE_HOMING/Marlin/Configuration_adv.h
+++ firmware/V1.1/Marlin-2.0.9.3_for_ender3_TMC2209+3dtouch+Z_SAFE_HOMING/Marlin/Configuration_adv.h
@@ -880,7 +880,7 @@
 
 // Safety: The probe needs time to recognize the command.
 //         Minimum command delay (ms). Enable and increase if needed.
-//#define BLTOUCH_DELAY 500
+#define BLTOUCH_DELAY 300
 
 /**
    * Settings for BLTOUCH Classic 1.2, 1.3 or BLTouch Smart 1.0, 2.0, 2.2, 3.0, 3.1, and most clones:
@@ -924,7 +924,7 @@
    *
    * Set the default state here, change with 'M401 S' or UI, use M500 to save, M502 to reset.
    */
-//#define BLTOUCH_HS_MODE true
+#define BLTOUCH_HS_MODE true
 
 // Safety: Enable voltage mode settings in the LCD menu.
 //#define BLTOUCH_LCD_VOLTAGE_MENU
@@ -1316,7 +1316,7 @@
 
 // Add Probe Z Offset calibration to the Z Probe Offsets menu
 #if HAS_BED_PROBE
-//#define PROBE_OFFSET_WIZARD
+#define PROBE_OFFSET_WIZARD
 #if ENABLED(PROBE_OFFSET_WIZARD)
 //
 // Enable to init the Probe Z-Offset when starting the Wizard.
@@ -1981,7 +1981,7 @@
  *
  * Warning: Does not respect endstops!
  */
-//#define BABYSTEPPING
+#define BABYSTEPPING
 #if ENABLED(BABYSTEPPING)
 //#define INTEGRATED_BABYSTEPPING         // EXPERIMENTAL integration of babystepping into the Stepper ISR
 //#define BABYSTEP_WITHOUT_HOMING
@@ -2004,7 +2004,7 @@
 
 //#define BABYSTEP_DISPLAY_TOTAL          // Display total babysteps since last G28
 
-//#define BABYSTEP_ZPROBE_OFFSET          // Combine M851 Z and Babystepping
+#define BABYSTEP_ZPROBE_OFFSET          // Combine M851 Z and Babystepping
 #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
 //#define BABYSTEP_HOTEND_Z_OFFSET      // For multiple hotends, babystep relative Z offsets
 //#define BABYSTEP_ZPROBE_GFX_OVERLAY   // Enable graphical overlay on Z-offset editor
