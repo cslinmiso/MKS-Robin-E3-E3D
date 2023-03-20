7dd7e80b5fa7a022a1fe1941a4d424102d17e9a6
weihsuehl <weihsuehl@yahooinc.com>
2023-03-20 13:48:04 +0800

    update

--- firmware/V1.1/Marlin-2.0.6.1_for_ender3_TMC2209+3dtouch+Z_SAFE_HOMING/Marlin/Configuration.h
+++ firmware/V1.1/Marlin-2.0.6.1_for_ender3_TMC2209+3dtouch+Z_SAFE_HOMING/Marlin/Configuration.h
@@ -993,7 +993,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -41, -14.4, -3}
+#define NOZZLE_TO_PROBE_OFFSET { -41, -14.4, -2.8}
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
