4dc6cd8f29430f598215a82cc6ccc872c90a5b93
weihsuehl <weihsuehl@yahooinc.com>
2023-03-31 13:36:30 +0800

    NOZZLE_TO_PROBE_OFFSET

--- firmware/V1.1/Marlin-2.0.9.3_for_ender3_TMC2209+3dtouch+Z_SAFE_HOMING/Marlin/Configuration.h
+++ firmware/V1.1/Marlin-2.0.9.3_for_ender3_TMC2209+3dtouch+Z_SAFE_HOMING/Marlin/Configuration.h
@@ -1181,14 +1181,14 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -41, -14.4, -2.9 }
+#define NOZZLE_TO_PROBE_OFFSET { -41, -14.4, -2.8 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
 #define PROBING_MARGIN 30
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_FEEDRATE (133*60)
+#define XY_PROBE_FEEDRATE (150*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_FEEDRATE_FAST (20*60)
@@ -1757,7 +1757,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_MM_M { (133 * 60), (133 * 60), (10 * 60) }
+#define HOMING_FEEDRATE_MM_M { (145 * 60), (145 * 60), (20 * 60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
